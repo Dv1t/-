b4ad3574d981c4dc8c7e3a2218ded2047ea7e90f
Dv1t <vitdrav@gmail.com>
2019-03-09 15:43:00 +0300

    5

---
+++ Array.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace _5
+{
+    class MatrixMultiply
+    {
+        static void Main(string[] args)
+        {
+            int[] a;
+            Console.Write("Введите размер массива: ");
+            int n = int.Parse(System.Console.ReadLine());
+            a = new int[n];
+            for(int i =0;i<n;i++)
+            {
+                Console.Write("Введите {0}-й элемент массива: ",i+1);
+                a[i] = int.Parse(System.Console.ReadLine());
+            }
+            int s=0,so=0,sp=0,sc=0,sn=0,max=-9999999,min=9999999,smin=0,imin=0,imax=0;
+           for(int i=0;i<n;i++)
+            {
+                if(a[i]>max)
+                {
+                    max = a[i];
+                    imax = i;
+                }
+                if (a[i] < min)
+                {
+                    min = a[i];
+                    imin = i;
+                }
+                s += a[i];
+                if(a[i]<0)
+                {
+                    so += a[i];
+                }
+                else
+                {
+                    sp += a[i];
+                }
+                if((i+1)%2==0)
+                {
+                    sc += a[i];
+                }
+                else
+                {
+                    sn += a[i];
+                }
+                
+            }
+           if(imin>imax)
+            {
+                for(int i = imax;i<=imin;i++)
+                {
+                    smin += a[i];
+                }
+            }
+           else
+            {
+                for (int i = imin; i <= imax; i++)
+                {
+                    smin += a[i];
+                }
+            }
+            float sr = (float)s / n;
+            Console.WriteLine("Сумма всех элементов массива = {0}", s);
+            Console.WriteLine("Сумма отрицательных элементов массива = {0}", so);
+            Console.WriteLine("Сумма положительных элементов массива = {0}", sp);
+            Console.WriteLine("Сумма элементов массива с чётными номерами = {0}", sc);
+            Console.WriteLine("Сумма элементов массива с нечётными номерами = {0}", sn);
+            Console.WriteLine("Среднее значение элементов массива = {0}", sr);
+            Console.WriteLine("Максимальное значение элементов массива = {0}", max);
+            Console.WriteLine("Сумма значений элементов массива между максимальеым и минимальным = {0}", smin);
+            Console.ReadKey();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _6
+namespace _5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Book b1 = new Book();
-            b1.SetBook("Пушкин А.С.", "Капитанская дочка", "Вильямс", 123, 2018);
-            Book.SetPrice(12);
-            b1.Print();
-            Console.WriteLine("\n Итоговая стоимость аренды: {0} p.", b1.PriceBook(3));
-            Book.SetPrice(5);
-            b1.Print();
-
-            Book b2 = new Book("Толстой Л.Н.", "Война и мир", "Наука и жизнь", 
-                1234, 2013);
-            b2.Print();
-
-            Book b3 = new Book("Лермонтов М.Ю.", "Мцыри");
-            b3.Print();
-           
-            Triangle t1 = new Triangle(1, 1, 100);
-            t1.Print();
-            t1.Perimetr();
-            t1.Square();
-
-            Console.ReadKey();
+            int[] myArray;
+            Console.WriteLine("Длина массива: ");
+            int n = int.Parse(Console.ReadLine());
+            myArray = new int[n];
+            for (int i = 0; i < myArray.Length; ++i)
+            {
+                Console.Write("a[{0}]=", i);
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int x in myArray) Console.Write("{0} ", x);
+            System.Threading.Thread.Sleep(2000);
         }
     }
 }
