2f69bde46660182a68b4c392fced070ff2e19ac1
Dv1t <vitdrav@gmail.com>
2019-03-09 15:45:17 +0300

    4

---
+++ Operation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Operation
+    {
+        private static bool CheckT(int a, int b, int c)
+        {
+            if (a + b < c)
+            {
+                return false;
+            }
+            if (a + c < b)
+            {
+                return false;
+            }
+            if (b + c < a)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static double GetS(int a, int b,int c)
+        {
+            if (CheckT(a, b, c))
+                {
+                float p = (a + b + c) / 2;
+                double s = p * (p - a) * (p - a) * (p - a);
+                s = Math.Sqrt(s);
+                return s;
+                }
+            else
+            {
+                return 0;
+            }
+           
+        }
+ 
+        public static double GetS(int a)
+        {
+            float p = (a * 3) / 2;
+            double s = p * (p - a) * (p - a) * (p - a);
+            s = Math.Sqrt(s);
+            return s;
+        }
+
+        public static int Roots(int a,int b,int c, ref float x1,ref float x2)
+        {
+            float d = b * b - 4 * a * c;
+            if(d<0)
+            {
+                return -1;
+            }
+            if(d==0)
+            {
+                x1 =(float) (b * -1) / (2 * a);
+                return 0;
+            }
+            if(d>0)
+            {
+                x1 = (float)((b * -1)+Math.Sqrt(d)) / (2 * a);
+                x2 = (float)((b * -1) - Math.Sqrt(d)) / (2 * a);
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,55 @@
 ﻿using System;
-namespace _5
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] myArray;
-            Console.WriteLine("Длина массива: ");
-            int n = int.Parse(Console.ReadLine());
-            myArray = new int[n];
-            for (int i = 0; i < myArray.Length; ++i)
+            int f;
+            bool ok;
+            Console.WriteLine("Number for factorial:");
+            int x = int.Parse(Console.ReadLine());
+            ok = Utils.Factorial(x, out f);
+            if (ok)
+                Console.WriteLine("Factorial(" + x + ") = " + f);
+            else
+                Console.WriteLine("Cannot compute this factorial");
+
+            Console.WriteLine("Введите первую сторону:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третью сторону:");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Площадь треугольник равна:{0}",Operation.GetS(a,b,c));
+
+            Console.WriteLine("Введите сторону:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Площадь треугольник равна:{0}", Operation.GetS(a));
+
+            Console.WriteLine("Введите коэффициенты:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            float x1=0, x2=0;
+            int result =  Operation.Roots(a, b, c,ref x1, ref x2);
+            if( result==-1)
             {
-                Console.Write("a[{0}]=", i);
-                myArray[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Корней уравнения с коэффициентами a = {0}, b = {1}, c = {2} нет.",a,b,c);
             }
-            foreach (int x in myArray) Console.Write("{0} ", x);
+            if(result==0)
+            {
+                Console.WriteLine("Корней уравнения с коэффициентами a = {0}, b = {1}, c = {2} равны один x1= x2 = {3}.", a, b, c,x1);
+            }
+            if (result == 1)
+            {
+                Console.WriteLine("Корней уравнения с коэффициентами a = {0}, b = {1}, c = {2} равны x1 = {2}, x2 = {3}.", a, b, c, x1,x2);
+            }
             System.Threading.Thread.Sleep(2000);
         }
     }
